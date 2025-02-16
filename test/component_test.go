13e03877112d030ce6f1a97f862bfd1c0c09b3d0
Igor Rodionov <goruha@gmail.com>
2025-02-16 22:31:49 +0100

    Added tests

--- test/component_test.go
+++ test/component_test.go
@@ -10,132 +10,110 @@ import (
 	"github.com/aws/aws-sdk-go-v2/service/sesv2"
 	"github.com/aws/aws-sdk-go-v2/service/sesv2/types"
 	"github.com/cloudposse/test-helpers/pkg/atmos"
-	helper "github.com/cloudposse/test-helpers/pkg/atmos/aws-component-helper"
-	"github.com/gruntwork-io/terratest/modules/aws"
+	helper "github.com/cloudposse/test-helpers/pkg/atmos/component-helper"
+	awshelper "github.com/cloudposse/test-helpers/pkg/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-func TestComponent(t *testing.T) {
-	// Define the AWS region to use for the tests
-	awsRegion := "us-east-2"
-
-	// Initialize the test fixture
-	fixture := helper.NewFixture(t, "../", awsRegion, "test/fixtures")
-
-	// Ensure teardown is executed after the test
-	defer fixture.TearDown()
-	fixture.SetUp(&atmos.Options{})
-
-	// Define the test suite
-	fixture.Suite("default", func(t *testing.T, suite *helper.Suite) {
-		// Setup phase: Create DNS zones for testing
-		suite.Setup(t, func(t *testing.T, atm *helper.Atmos) {
-			basicDomain := "components.cptest.test-automation.app"
-
-			// Deploy the delegated DNS zone
-			inputs := map[string]interface{}{
-				"zone_config": []map[string]interface{}{
-					{
-						"subdomain": suite.GetRandomIdentifier(),
-						"zone_name": basicDomain,
-					},
-				},
-			}
-			atm.GetAndDeploy("dns-delegated", "default-test", inputs)
-		})
-
-		// Teardown phase: Destroy the DNS zones created during setup
-		suite.TearDown(t, func(t *testing.T, atm *helper.Atmos) {
-			// Deploy the delegated DNS zone
-			inputs := map[string]interface{}{
-				"zone_config": []map[string]interface{}{
-					{
-						"subdomain": suite.GetRandomIdentifier(),
-						"zone_name": "components.cptest.test-automation.app",
-					},
-				},
-			}
-			atm.GetAndDestroy("dns-delegated", "default-test", inputs)
-		})
-
-		// Test phase: Validate the functionality of the ALB component
-		suite.Test(t, "basic", func(t *testing.T, atm *helper.Atmos) {
-			dnsDelegatedComponent := helper.NewAtmosComponent("dns-delegated", "default-test", map[string]interface{}{})
-			domain := atm.Output(dnsDelegatedComponent, "default_domain_name")
-
-			hostnamePrefix := strings.ToLower(random.UniqueId())
-			inputs := map[string]interface{}{
-				"domain_template": hostnamePrefix + "-%[3]v.%[2]v.%[1]v." + domain,
-				"ssm_prefix":      fmt.Sprintf("/ses/%s", hostnamePrefix),
-			}
-			defer atm.GetAndDestroy("ses/basic", "default-test", inputs)
-			component := atm.GetAndDeploy("ses/basic", "default-test", inputs)
-			assert.NotNil(t, component)
-
-			smtpPassword := atm.Output(component, "smtp_password")
-			assert.NotEmpty(t, smtpPassword)
-
-			smtpUser := atm.Output(component, "smtp_user")
-			assert.NotEmpty(t, smtpUser)
-
-			userUniqueId := atm.Output(component, "user_unique_id")
-			assert.NotEmpty(t, userUniqueId)
-
-			userArn := atm.Output(component, "user_arn")
-			assert.NotEmpty(t, userArn)
-
-			randomContentMessage := strings.ToLower(random.UniqueId())
-
-			indentityDomain := fmt.Sprintf("%s-test.ue2.default.%s", hostnamePrefix, domain)
-			senderEmail := fmt.Sprintf("test@%s", indentityDomain)
-			client := NewSESV2Client(t, awsRegion)
-
-			var identityState string
-			var attempt int = 0
-			for strings.ToLower(identityState) != "success" && attempt <= 60 {
-				indenties, err := client.GetEmailIdentity(context.Background(), &sesv2.GetEmailIdentityInput{
-					EmailIdentity: &indentityDomain,
-				})
-				assert.NoError(t, err)
-
-				identityState = string(indenties.VerificationStatus)
-				time.Sleep(2 * time.Second)
-				attempt++
-			}
-			assert.Equal(t, "SUCCESS", identityState)
-
-			_, err := client.SendEmail(context.Background(), &sesv2.SendEmailInput{
-				Content: &types.EmailContent{
-					Raw: &types.RawMessage{
-						Data: []byte(fmt.Sprintf("Test email %s", randomContentMessage)),
-					},
-				},
-				Destination: &types.Destination{
-					// https://docs.aws.amazon.com/ses/latest/dg/send-an-email-from-console.html
-					ToAddresses: []string{"success@simulator.amazonses.com"},
-				},
-				FromEmailAddress: &senderEmail,
-			})
-			assert.NoError(t, err)
-		})
-	})
+type ComponentSuite struct {
+	helper.TestSuite
 }
 
-// NewElbV2Client creates en ELB client.
-func NewSESV2Client(t *testing.T, region string) *sesv2.Client {
-	client, err := NewSESV2ClientE(t, region)
-	require.NoError(t, err)
+func (s *ComponentSuite) TestBasic() {
+	const component = "ses/basic"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
 
-	return client
-}
+	dnsDelegatedOptions := s.GetAtmosOptions("dns-delegated", stack, nil)
+	domain := atmos.Output(s.T(), dnsDelegatedOptions, "default_domain_name")
 
-// NewElbV2ClientE creates an ELB client.
-func NewSESV2ClientE(t *testing.T, region string) (*sesv2.Client, error) {
-	sess, err := aws.NewAuthenticatedSession(region)
-	if err != nil {
-		return nil, err
+	hostnamePrefix := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"domain_template": hostnamePrefix + "-%[3]v.%[2]v.%[1]v." + domain,
+		"ssm_prefix":      fmt.Sprintf("/ses/%s", hostnamePrefix),
 	}
-	return sesv2.NewFromConfig(*sess), nil
+	defer s.DestroyAtmosComponent(s.T(), component, stack, &inputs)
+	options, _ := s.DeployAtmosComponent(s.T(), component, stack, &inputs)
+	assert.NotNil(s.T(), options)
+
+	smtpPassword := atmos.Output(s.T(), options, "smtp_password")
+	assert.NotEmpty(s.T(), smtpPassword)
+
+	smtpUser := atmos.Output(s.T(), options, "smtp_user")
+	assert.NotEmpty(s.T(), smtpUser)
+
+	userUniqueId := atmos.Output(s.T(), options, "user_unique_id")
+	assert.NotEmpty(s.T(), userUniqueId)
+
+	userArn := atmos.Output(s.T(), options, "user_arn")
+	assert.NotEmpty(s.T(), userArn)
+
+	randomContentMessage := strings.ToLower(random.UniqueId())
+	identityDomain := fmt.Sprintf("%s-test.ue2.default.%s", hostnamePrefix, domain)
+	senderEmail := fmt.Sprintf("test@%s", identityDomain)
+	client := awshelper.NewSESV2Client(s.T(), awsRegion)
+
+	var identityState string
+	var attempt int
+	for strings.ToLower(identityState) != "success" && attempt <= 60 {
+		identities, err := client.GetEmailIdentity(context.Background(), &sesv2.GetEmailIdentityInput{
+			EmailIdentity: &identityDomain,
+		})
+		assert.NoError(s.T(), err)
+
+		identityState = string(identities.VerificationStatus)
+		time.Sleep(2 * time.Second)
+		attempt++
+	}
+	assert.Equal(s.T(), "SUCCESS", identityState)
+
+	_, err := client.SendEmail(context.Background(), &sesv2.SendEmailInput{
+		Content: &types.EmailContent{
+			Raw: &types.RawMessage{
+				Data: []byte(fmt.Sprintf("Test email %s", randomContentMessage)),
+			},
+		},
+		Destination: &types.Destination{
+			ToAddresses: []string{"success@simulator.amazonses.com"},
+		},
+		FromEmailAddress: &senderEmail,
+	})
+	assert.NoError(s.T(), err)
+
+	s.DriftTest(component, stack, &inputs)
+}
+
+func (s *ComponentSuite) TestEnabledFlag() {
+	const component = "ses/disabled"
+	const stack = "default-test"
+	const awsRegion = "us-east-2"
+
+	dnsDelegatedOptions := s.GetAtmosOptions("dns-delegated", stack, nil)
+	domain := atmos.Output(s.T(), dnsDelegatedOptions, "default_domain_name")
+
+	hostnamePrefix := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"domain_template": hostnamePrefix + "-%[3]v.%[2]v.%[1]v." + domain,
+		"ssm_prefix":      fmt.Sprintf("/ses/%s", hostnamePrefix),
+	}
+
+	s.VerifyEnabledFlag(component, stack, &inputs)
+}
+
+
+func TestRunSuite(t *testing.T) {
+	suite := new(ComponentSuite)
+
+	subdomain := strings.ToLower(random.UniqueId())
+	inputs := map[string]interface{}{
+		"zone_config": []map[string]interface{}{
+			{
+				"subdomain": subdomain,
+				"zone_name": "components.cptest.test-automation.app",
+			},
+		},
+	}
+	suite.AddDependency(t, "dns-delegated", "default-test", &inputs)
+	helper.Run(t, suite)
 }
