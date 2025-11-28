baec02ea87fee860240bbbe5e3891d85286a5a52
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-11-28 17:25:37 +0300

    Update variables-account-map.tf

--- src/variables-account-map.tf
+++ src/variables-account-map.tf
@@ -1,6 +1,6 @@
 variable "account_map_enabled" {
   type        = bool
-  description = "INFO: Temporary variable required for account-map deprication plan. Please do not change the value"
+  description = "INFO: Temporary variable required for account-map deprecation plan. Please do not change the value"
   default     = true
 }
 
@@ -13,7 +13,7 @@ variable "account_map" {
     aws_partition                 = optional(string, "aws")
     iam_role_arn_templates        = optional(map(string), {})
   })
-  description = "INFO: Temporary variable required for account-map deprication plan. Please do not change the value"
+  description = "INFO: Temporary variable required for account-map deprecation plan. Please do not change the value"
   default = {
     full_account_map              = {}
     audit_account_account_name    = ""
