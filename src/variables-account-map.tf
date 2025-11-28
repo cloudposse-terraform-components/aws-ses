613cddcc66e3b703648c757c8fa95d4d4b62e6eb
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-11-28 16:30:11 +0300

    Create account_map and account_map_enabled variables
    Added temporary variables for account-map deprecation plan.

---
+++ src/variables-account-map.tf
@@ -0,0 +1,25 @@
+variable "account_map_enabled" {
+  type        = bool
+  description = "INFO: Temporary variable required for account-map deprication plan. Please do not change the value"
+  default     = true
+}
+
+variable "account_map" {
+  type = object({
+    full_account_map              = map(string)
+    audit_account_account_name    = optional(string, "")
+    root_account_account_name     = optional(string, "")
+    identity_account_account_name = optional(string, "")
+    aws_partition                 = optional(string, "aws")
+    iam_role_arn_templates        = optional(map(string), {})
+  })
+  description = "INFO: Temporary variable required for account-map deprication plan. Please do not change the value"
+  default = {
+    full_account_map              = {}
+    audit_account_account_name    = ""
+    root_account_account_name     = ""
+    identity_account_account_name = ""
+    aws_partition                 = "aws"
+    iam_role_arn_templates        = {}
+  }
+}
