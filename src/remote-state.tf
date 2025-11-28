8dcd0ab82c8ea94bceab730268f6375bd5ab7fe1
Igor Rodionov <496956+goruha@users.noreply.github.com>
2025-11-28 15:29:07 +0100

    Refactor environment variable assignment in remote-state (#50)
    * Refactor environment variable assignment in remote-state
    
    * Create account_map and account_map_enabled variables
    
    Added temporary variables for account-map deprecation plan.
    
    * Update variables-account-map.tf

--- src/remote-state.tf
+++ src/remote-state.tf
@@ -1,9 +1,13 @@
+locals {
+  dns_delegated_environment_name = var.account_map_enabled ? module.iam_roles.global_environment_name : module.this.context.environment
+}
+
 module "dns_gbl_delegated" {
   source  = "cloudposse/stack-config/yaml//modules/remote-state"
   version = "1.8.0"
 
   component   = "dns-delegated"
-  environment = coalesce(var.dns_delegated_environment_name, module.iam_roles.global_environment_name)
+  environment = coalesce(var.dns_delegated_environment_name, local.dns_delegated_environment_name)
 
   context = module.this.context
 }
